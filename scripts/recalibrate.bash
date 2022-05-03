658af8b149dcb6a8458380a94f05d8f7ef1b5c2a
Abigail Howell <aahowel3@asu.edu>
2022-05-03 11:34:35 -0700

    removed recal script

--- scripts/recalibrate.bash
+++
@@ -1,9 +0,0 @@
-set -e
-
-GATK=/usr/bin/gatk
-GATK_ARGS=GenotypeGVCFs \
- -nt 5 \
- --disable_auto_index_creation_and_locking_when_reading_rods 
-REF=../data/ref_genome/mac_mito.fasta
-
-$GATK $GATK_ARGS -R $REF -V `find ../data/variant_calls/ -name *.vcf | xargs | sed 's/ / -V /g'` -o tetrahymena.gvcf.merged.vcf
