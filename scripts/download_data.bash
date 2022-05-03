c6431e6f5a7d611291a9e56efdc7db70a313b64b
Abigail Howell <aahowel3@asu.edu>
2022-05-03 11:30:17 -0700

    moved bash downloading commands to script

---
+++ scripts/download_data.bash
@@ -0,0 +1,7 @@
+#2020 MAC reference (includes rDNA chromosome chr_181)
+curl -o ../data/ref_genome/1-upd-Genome-assembly.fasta http://www.ciliate.org/system/downloads/1-upd-Genome-assembly.fasta
+#mitchondrial reference
+esearch -db nucleotide -query "NC_003029.1" | efetch -format fasta > ../data/ref_genome/NC_003029.1.fasta
+#combine and index
+cat ../data/ref_genome/1-upd-Genome-assembly.fasta ../data/ref_genome/NC_003029.1.fasta > ../data/ref_genome/mac_mito.fasta
+bwa index ../data/ref_genome/mac_mito.fasta
