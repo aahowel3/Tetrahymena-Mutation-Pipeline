93cdf4cc9d842aabf7aa412693ebd08300cf0af1
Abigail Howell <aahowel3@asu.edu>
2022-05-31 16:43:34 -0700

    Update create_config.bash

--- scripts/create_config.bash
+++ scripts/create_config.bash
@@ -9,3 +9,11 @@ echo "BASENAMES=\\"
 cat $metadata | tail -n +2 | cut -f 2 |
   sed -re 's|_L00[1234]$||' | sort | uniq | awk '{ print "    ", $0, "\\" }'
 echo ""
+
+echo "BASENAME_MIC_FILES=\\"
+cat $metadata | tail -n +2 | cut -f 2 |  uniq | grep -e "Anc" -e "GE" | awk '{ print "    ", $0, "\\" }'
+echo ""
+
+echo "BASENAME_MAC_FILES=\\"
+cat $metadata | tail -n +2 | cut -f 2 |  uniq | grep -e "MA" -e "SB210" | awk '{ print "    ", $0, "\\" }'
+echo ""
