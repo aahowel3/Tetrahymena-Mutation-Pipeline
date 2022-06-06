1df369bc0722baeb8366620cda1cc2a26fdf08fa
Abigail Howell <aahowel3@asu.edu>
2022-06-06 12:24:29 -0700

    Update merge_lanes.bash

--- scripts/merge_lanes.bash
+++ scripts/merge_lanes.bash
@@ -1,17 +1,11 @@
 set -e
-
-PICARD=/usr/bin/picard
-PICARD_ARGS=MergeSamFiles \
- USE_THREADING=true \
- MAX_RECORDS_IN_RAM=2000000 \
- VALIDATION_STRINGENCY=SILENT \
- SORT_ORDER=queryname 
+SAMTOOLS_BIN="samtools"
 
 #input files
-B1="$1"
-B2="$2"
-B3="$3"
-B4="$4"
-OUTBAM="$5"
+OUTBAM="$1"
+B1="$2"
+B2="$3"
+B3="$4"
+B4="$5"
 
-$PICARD $PICARD_ARGS INPUT="$B1" INPUT="$B2" INPUT="$B3" INPUT="$B4" OUTPUT="$OUTBAM"
+"$SAMTOOLS_BIN" merge "$OUTBAM" "$B1" "$B2" "$B3" "$B4" 
