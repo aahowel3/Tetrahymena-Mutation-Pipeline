ca0de3b592573580fe12560ebc2774e0b01311f6
Abigail Howell <aahowel3@asu.edu>
2022-06-06 12:12:13 -0700

    Update create_config.bash

--- scripts/create_config.bash
+++ scripts/create_config.bash
@@ -1,16 +1,16 @@
-metadata=metadata.tsv
-dict=data/ref_genome/mac_mito.dict
+METADATA="$1"
+DICT="$2"
 
 echo "BASENAMES_WITH_LANES=\\"
-cat $metadata | tail -n +2 | cut -f 2 |
+cat "$METADATA" | tail -n +2 | cut -f 2 |
   uniq | awk '{ print "    ", $0, "\\" }'
 echo ""
 
 echo "BASENAMES=\\"
-cat $metadata | tail -n +2 | cut -f 2 |
+cat "$METADATA"  | tail -n +2 | cut -f 2 |
   sed -re 's|_L00[1234]$||' | sort | uniq | awk '{ print "    ", $0, "\\" }'
 echo ""
 
 echo "CONTIGS=\\"
-cat $dict | grep "^@SQ" |cut -f 2 | sed -e 's/^.*://g' | awk '{ print "    ", $0, "\\" }'
+cat "$DICT" | grep "^@SQ" |cut -f 2 | sed -e 's/^.*://g' | awk '{ print "    ", $0, "\\" }'
 echo ""
