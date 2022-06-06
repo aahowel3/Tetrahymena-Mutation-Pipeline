2de3f7a5359e31cb226f9812f7fef191ee7f32a3
Abigail Howell <aahowel3@asu.edu>
2022-06-06 12:16:58 -0700

    Update download_data.bash

--- scripts/download_data.bash
+++ scripts/download_data.bash
@@ -1,3 +1,4 @@
+PICARD=/usr/bin/picard
 #2020 MAC reference (includes rDNA chromosome chr_181)
 curl -o ../data/ref_genome/1-upd-Genome-assembly.fasta http://www.ciliate.org/system/downloads/1-upd-Genome-assembly.fasta
 #mitchondrial reference
@@ -5,4 +6,4 @@ esearch -db nucleotide -query "NC_003029.1" | efetch -format fasta > ../data/ref
 #combine and index
 cat ../data/ref_genome/1-upd-Genome-assembly.fasta ../data/ref_genome/NC_003029.1.fasta > ../data/ref_genome/mac_mito.fasta
 bwa index ../data/ref_genome/mac_mito.fasta
-picard CreateSequenceDictionary R=../data/ref_genome/mac_mito.fasta O=../data/ref_genome/mac_mito.dict
+$PICARD CreateSequenceDictionary R=../data/ref_genome/mac_mito.fasta O=../data/ref_genome/mac_mito.dict
