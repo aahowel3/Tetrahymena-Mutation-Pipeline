d322b77915d4c35ca6ac76f1ae74fdebbd916537
Abigail Howell <aahowel3@asu.edu>
2022-06-27 13:50:32 -0700

    scripts and makefile updated

--- scripts/haplotypecaller.bash
+++ scripts/haplotypecaller.bash
@@ -1,27 +1,10 @@
 set -e 
 
-GATK=/usr/bin/gatk
-GATK_ARGS="HaplotypeCaller \
---genotyping_mode DISCOVERY \
- -A AlleleBalanceBySample \
- -A DepthPerAlleleBySample \
- -A DepthPerSampleHC \
- -A InbreedingCoeff \
- -A MappingQualityZeroBySample \
- -A StrandBiasBySample \
- -A Coverage \
- -A FisherStrand \
- -A HaplotypeScore \
- -A MappingQualityRankSumTest \
- -A MappingQualityZero \
- -A QualByDepth \
- -A RMSMappingQuality \
- -A ReadPosRankSumTest \
- -A VariantType" 
+GATK=/home/aahowel3/miniconda3/envs/gatk4/bin/gatk
 
 REF="$1"
 BAM="$2"
 VCF="$3"
 
-"$GATK" "$GATK_ARGS" -R "$REF" -I "$BAM" -stand-call-conf 0 -ploidy 2 -O "$VCF"
+"$GATK" HaplotypeCaller -A DepthPerAlleleBySample -A DepthPerSampleHC -A InbreedingCoeff -A StrandBiasBySample -A Coverage -A FisherStrand -A MappingQualityRankSumTest -A MappingQualityZero -A QualByDepth -A RMSMappingQuality -A ReadPosRankSumTest -R "$REF" -I "$BAM" --mapping-quality-threshold-for-genotyping 0 -stand-call-conf 0 -ploidy 2 -O "$VCF"
 
